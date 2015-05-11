4385af6ce16938f9d2e3c0371b4652ab036f77e7
Jim Witschey <jim.witschey@gmail.com>
2015-05-11 11:09:37 -0400

    pulls out reusable test logic
    In particular, pulls out cqlshlib importing code and a prepare() method

--- cqlsh_tests/cqlsh_copy_tests.py
+++ cqlsh_tests/cqlsh_copy_tests.py
@@ -1,4 +1,5 @@
 import codecs
+from contextlib import contextmanager
 import csv
 import locale
 import os
@@ -16,20 +17,26 @@ from cqlsh_tools import DummyColorMap
 
 class CqlshCopyTest(Tester):
 
-    def result_to_csv_rows(self, result, cassandra_dir=None):
+    @contextmanager
+    def _cqlshlib(self):
+        saved_path = list(sys.path)
+        cassandra_dir = self.cluster.nodelist()[0].get_install_dir()
+
+        try:
+            sys.path = sys.path + [os.path.join(cassandra_dir, 'pylib')]
+            import cqlshlib
+            yield cqlshlib
+        finally:
+            sys.path = saved_path
+
+    def result_to_csv_rows(self, result):
         '''
         Given an object returned from a CQL query, returns a string formatted by
         the cqlsh formatting utilities.
         '''
         # unfortunately, it's not easy to grab the formatting function, so we
         # have to manually add its definition to the import path. Mega-gross.
-        saved_path = list(sys.path)
-        formatted = sigil = object()
-        if cassandra_dir is None:
-            cassandra_dir = self.cluster.nodelist()[0].get_install_dir()
-
-        try:
-            sys.path = sys.path + [os.path.join(cassandra_dir, 'pylib')]
+        with self._cqlshlib() as cqlshlib:
             from cqlshlib.formatting import format_value
             encoding_name = codecs.lookup(locale.getpreferredencoding()).name
 
@@ -40,20 +47,18 @@ class CqlshCopyTest(Tester):
                                     float_precision=None,
                                     colormap=DummyColorMap(),
                                     nullval=None).strval
-            formatted = [[fmt(v) for v in row] for row in result]
-        finally:
-            sys.path = saved_path
 
-        # if the try block failed, return None
-        return None if formatted is sigil else formatted
+        return [[fmt(v) for v in row] for row in result]
+
+    def prepare(self):
+        self.cluster.populate(1).start(wait_for_binary_proto=True)
+        self.node1, = self.cluster.nodelist()
+        self.session = self.patient_cql_connection(self.node1)
+        self.create_ks(self.session, 'ks', 1)
 
     def test_copy_to(self):
-        self.cluster.populate(1).start(wait_for_binary_proto=True)
-        node1, = self.cluster.nodelist()
-
-        session = self.patient_cql_connection(node1)
-        self.create_ks(session, 'ks', 1)
-        session.execute("""
+        self.prepare()
+        self.session.execute("""
             CREATE TABLE testcopyto (
                 a int,
                 b text,
@@ -62,45 +67,42 @@ class CqlshCopyTest(Tester):
                 PRIMARY KEY (a, b)
             )""")
 
-        insert_statement = session.prepare("INSERT INTO testcopyto (a, b, c, d) VALUES (?, ?, ?, ?)")
+        insert_statement = self.session.prepare("INSERT INTO testcopyto (a, b, c, d) VALUES (?, ?, ?, ?)")
         args = [(i, str(i), float(i) + 0.5, uuid4()) for i in range(1000)]
-        execute_concurrent_with_args(session, insert_statement, args)
+        execute_concurrent_with_args(self.session, insert_statement, args)
 
-        results = list(session.execute("SELECT * FROM testcopyto"))
+        results = list(self.session.execute("SELECT * FROM testcopyto"))
 
         tempfile = NamedTemporaryFile()
         debug('Exporting to csv file: {name}'.format(name=tempfile.name))
-        node1.run_cqlsh(cmds="COPY ks.testcopyto TO '{name}'".format(name=tempfile.name))
+        self.node1.run_cqlsh(cmds="COPY ks.testcopyto TO '{name}'".format(name=tempfile.name))
 
         self.assertSequenceEqual(list(csv_rows(tempfile.name)),
                                  list(self.result_to_csv_rows(results)))
 
         # import the CSV file with COPY FROM
-        session.execute("TRUNCATE ks.testcopyto")
-        node1.run_cqlsh(cmds="COPY ks.testcopyto FROM '{name}'".format(name=tempfile.name))
-        new_results = list(session.execute("SELECT * FROM testcopyto"))
+        self.session.execute("TRUNCATE ks.testcopyto")
+        self.node1.run_cqlsh(cmds="COPY ks.testcopyto FROM '{name}'".format(name=tempfile.name))
+        new_results = list(self.session.execute("SELECT * FROM testcopyto"))
         self.assertEqual(results, new_results)
 
     def test_list_data(self):
-        self.cluster.populate(1).start(wait_for_binary_proto=True)
-        [node] = self.cluster.nodelist()
-        session = self.patient_cql_connection(node)
-        self.create_ks(session, 'ks', 1)
-        session.execute("""
+        self.prepare()
+        self.session.execute("""
             CREATE TABLE testlist (
                 a int PRIMARY KEY,
                 b list<uuid>
             )""")
 
-        insert_statement = session.prepare("INSERT INTO testlist (a, b) VALUES (?, ?)")
+        insert_statement = self.session.prepare("INSERT INTO testlist (a, b) VALUES (?, ?)")
         args = [(i, random_list(gen=uuid4)) for i in range(1000)]
-        execute_concurrent_with_args(session, insert_statement, args)
+        execute_concurrent_with_args(self.session, insert_statement, args)
 
-        results = list(session.execute("SELECT * FROM testlist"))
+        results = list(self.session.execute("SELECT * FROM testlist"))
 
         tempfile = NamedTemporaryFile()
         debug('Exporting to csv file: {name}'.format(name=tempfile.name))
-        node.run_cqlsh(cmds="COPY ks.testlist TO '{name}'".format(name=tempfile.name))
+        self.node1.run_cqlsh(cmds="COPY ks.testlist TO '{name}'".format(name=tempfile.name))
 
         # csv_and_result_rows = zip(csv_rows(tempfile.name),
         #                           self.result_to_csv_rows(results))
@@ -111,25 +113,22 @@ class CqlshCopyTest(Tester):
                                  list(self.result_to_csv_rows(results)))
 
     def test_tuple_data(self):
-        self.cluster.populate(1).start(wait_for_binary_proto=True)
-        [node] = self.cluster.nodelist()
-        session = self.patient_cql_connection(node)
-        self.create_ks(session, 'ks', 1)
-        session.execute("""
+        self.prepare()
+        self.session.execute("""
             create table testlist (
                 a int primary key,
                 b tuple<uuid, uuid, uuid>
             )""")
 
-        insert_statement = session.prepare("insert into testtuple (a, b) values (?, ?)")
+        insert_statement = self.session.prepare("insert into testtuple (a, b) values (?, ?)")
         args = [(i, random_list(gen=uuid4, n=3)) for i in range(1000)]
-        execute_concurrent_with_args(session, insert_statement, args)
+        execute_concurrent_with_args(self.session, insert_statement, args)
 
-        results = list(session.execute("select * from testtuple"))
+        results = list(self.session.execute("select * from testtuple"))
 
         tempfile = NamedTemporaryFile()
         debug('exporting to csv file: {name}'.format(name=tempfile.name))
-        node.run_cqlsh(cmds="copy ks.testtuple to '{name}'".format(name=tempfile.name))
+        self.node1.run_cqlsh(cmds="copy ks.testtuple to '{name}'".format(name=tempfile.name))
 
         self.assertsequenceequal(list(csv_rows(tempfile.name)),
                                  list(self.result_to_csv_rows(results)))
