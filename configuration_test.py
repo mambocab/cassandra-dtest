b2dee998241d7306e4a50670ebeec8c646ebc7df
Jim Witschey <jim.witschey@gmail.com>
2015-06-08 13:01:09 -0400

    adds test for durable_writes caching bug

--- configuration_test.py
+++ configuration_test.py
@@ -1,6 +1,8 @@
 from dtest import Tester
 
 import ast
+import time
+from jmxutils import make_mbean, JolokiaAgent
 
 class TestConfiguration(Tester):
 
@@ -24,6 +26,38 @@ class TestConfiguration(Tester):
         cursor.execute( alter_chunk_len_query.format(chunk_length=64) )
         self._check_chunk_length( cursor, 64 )
 
+    def change_durable_writes_test(self):
+        """
+        @jira_ticket 9560
+        """
+        # We want writes to block on commitlog fsync
+        self.cluster.set_configuration_options(batch_commitlog=True)
+        self.cluster.populate(1).start(wait_for_binary_proto=True)
+        node = self.cluster.nodelist()[0]
+        cursor = self.patient_cql_connection(node)
+
+        # commitlog_size_mbean = make_mbean('metrics', type='CommitLog', name='TotalCommitLogSize', keyspace='ks')
+        commitlog_size_mbean = make_mbean('metrics', type='CommitLog', name='TotalCommitLogSize')
+        def commit_log_size():
+            with JolokiaAgent(node) as jmx:
+                return jmx.read_attribute(commitlog_size_mbean, 'Value')
+
+        init_size = commit_log_size()
+
+        cursor.execute("CREATE KEYSPACE ks WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1} "
+                       "AND DURABLE_WRITES = false")
+        cursor.execute('CREATE TABLE ks.tab (key int PRIMARY KEY, a int)')
+        cursor.execute('INSERT INTO ks.tab (key, a) VALUES (1, 4)')
+
+        self.assertEqual(init_size, commit_log_size())
+
+        cursor.execute('ALTER KEYSPACE WITH WITH DURABLE_WRITES = true')
+
+        self.assertEqual(init_size, commit_log_size())
+
+        cursor.execute('INSERT INTO ks.tab (key, a) VALUES (2, 5)')
+
+        self.assertLess(init_size, commit_log_size())
 
     def _check_chunk_length(self, cursor, value):
         describe_table_query = "SELECT * FROM system.schema_columnfamilies WHERE keyspace_name='ks' AND columnfamily_name='test_table';"
