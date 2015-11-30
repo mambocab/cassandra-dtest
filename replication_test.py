0ba5fe7673b101b17981ef9f02c53f36af17ab1c
Jim Witschey <jim.witschey@gmail.com>
2015-11-30 12:18:43 -0500

    set ignore_log_patterns at class level

--- replication_test.py
+++ replication_test.py
@@ -259,11 +259,9 @@ class SnitchConfigurationUpdateTest(Tester):
     Test to repro CASSANDRA-10238, wherein changing snitch properties to change racks without a restart could violate RF contract.
     """
 
-    def __init__(self, *args, **kwargs):
-        Tester.__init__(self, *args, **kwargs)
-        self.ignore_log_patterns = ["Fatal exception during initialization",
-                                    "Cannot start node if snitch's rack(.*) differs from previous rack(.*)",
-                                    "Cannot update data center or rack"]
+    ignore_log_patterns = ["Fatal exception during initialization",
+                           "Cannot start node if snitch's rack(.*) differs from previous rack(.*)",
+                           "Cannot update data center or rack"]
 
     def check_endpoint_count(self, ks, table, nodes, rf):
         """
