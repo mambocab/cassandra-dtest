05ab5c806e216bb203aecf97b6e13ee32f4b430a
Jim Witschey <jim.witschey@gmail.com>
2016-02-11 12:50:57 -0500

    don't just grab sys.argv in nose.main anymore

--- runner.py
+++ runner.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """
-Usage runner.py [--vnodes VNODES...]
+Usage: runner.py [--nose-option NOSE_OPTIONS] [TESTS...]
 
---nose-option NOSE_OPTIONS...  specify options to pass to `nosetests`.
+--nose-option NOSE_OPTIONS  specify options to pass to `nosetests`.
+TESTS                       space-separated list of tests to pass to `nosetests`
 """
 from __future__ import print_function
 
@@ -26,5 +27,9 @@ def validate_and_serialize_options():
 if __name__ == '__main__':
     options = docopt(__doc__)
 
+    nose_option_list = options['--nose-option'].split()
+    test_list = options['TESTS']
+    nose_argv = nose_option_list + test_list
+
     nose.main(addplugins=[dtestconfig.DtestConfigPlugin()],
-              argv=options['--nose-option'])
+              argv=nose_argv)
